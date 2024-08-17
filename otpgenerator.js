f3d75a5c42bac152333d4c738603cab9e06a1c9a
annawarpe <156175611+annawarpe@users.noreply.github.com>
2024-08-17 08:56:10 -0700

    otpgenerator.js

---
+++ otpgenerator.js
@@ -0,0 +1,10 @@
+function otpgenerate(length)
+{
+    let otp="";
+    for(let i=0;i<length;i++)
+    {
+        otp += Math.floor(Math.random() * 10).toString();
+    }
+    return otp;
+}
+console.log(otpgenerate(6));
